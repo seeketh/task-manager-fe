d52359389c60a769eef8df58f5084d41d01030c5
Nsubis Genesis <nsubisi@gmail.com>
2023-04-15 04:41:51 +0300

    Adjusted Authbar Background

--- src/components/AuthBar.js
+++ src/components/AuthBar.js
@@ -9,7 +9,7 @@ const AuthBar = () => {
     const { auth } = useSelector(store => store);
 
     return (
-        <div className="w-full font-bold bg-gray-600 dark:bg-slate-700 text-slate-400 flex justify-end items-center divide-x divide-slate-500">
+        <div className="w-full font-bold bg-gray-500 dark:bg-slate-500 text-slate-400 flex justify-end items-center divide-x divide-slate-500">
             <div className="p-2">{ auth.name }</div>
             {/*<div className="p-2">
                 <BellAlert />
